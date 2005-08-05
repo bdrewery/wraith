0258eaa21024b6f6252a2dd9e4207d8f148edbaf
Bryan Drewery <bryan@shatow.net>
2005-08-05 22:35:48 +0000

    svn: 2414

--- src/main.c
+++ src/main.c
@@ -379,7 +379,7 @@ static void dtx_arg(int argc, char *argv[])
 	printf("%s\nBuild Date: %s (%s%lu%s)\n", version, date, BOLD(-1), buildts, BOLD_END(-1));
         printf("BuildOS: %s%s%s BuildArch: %s%s%s\n", BOLD(-1), BUILD_OS, BOLD_END(-1), BOLD(-1), BUILD_ARCH, BOLD_END(-1));
 
-	printf("pack: %d conf: %d settings_t: %d pad: %d\n", SIZE_PACK, SIZE_CONF, sizeof(settings_t), SIZE_PAD);
+	sdprintf("pack: %d conf: %d settings_t: %d pad: %d\n", SIZE_PACK, SIZE_CONF, sizeof(settings_t), SIZE_PAD);
         if (settings.uname[0]) {
           sdebug++;
           bin_to_conf();
