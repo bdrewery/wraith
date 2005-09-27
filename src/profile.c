b02996f0b93998c3f3e92859d664155243fc5fc0
Bryan Drewery <bryan@shatow.net>
2005-09-27 03:50:28 +0000

    svn: 2461

--- src/profile.c
+++ src/profile.c
@@ -7,14 +7,18 @@
 #ifdef DEBUG
 #include "common.h"
 #include "hash_table.h"
+#include <sys/times.h>
+#include <signal.h>
+
 
 double gettime(clock_t start)
 {
-  clock_t end;
-  double cpu_time_used;
+  clock_t end = times(NULL);
+  clock_t diff = end - start;
 
-  end = clock();
-  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+  double cpu_time_used = (double) ((double) diff / 100.00);
+//  double cpu_time_used = ((double) diff / 100);
+
   return cpu_time_used;
 }
 
@@ -34,11 +38,12 @@ static int my_walk(const void *key, void *dataptr, void *param)
 
 void profile(int argc, char **argv)
 {
+  clock_t p_start = times(NULL);
   hash_table_t *ht = NULL;
   clock_t start;
   double total;
 
-  start = clock();
+  start = times(NULL);
   ht = hash_table_create(NULL, NULL, 100, HASH_TABLE_STRINGS);
   printf("Hash table created with %d rows\n", ht->max_rows);
   hash_table_insert(ht, "key1", (void *) "data1");
@@ -52,19 +57,9 @@ void profile(int argc, char **argv)
 
   display_results(start, "hash table", 6);
 
+  sleep(3);
+
+  printf("END: %.12fs\n", gettime(p_start));
   exit(0);
 }
 #endif /* DEBUG */
-
-
-
-
-
-
-
-
-
-
-
-
-
