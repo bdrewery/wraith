e16e0dfa8d19a9aff0a5ba59b496043c9cc2d5fc
Bryan Drewery <bryan@shatow.net>
2005-10-05 23:04:54 +0000

    * Fixed segfault in msgc_help
    svn: 2476

--- src/mod/irc.mod/msgcmds.c
+++ src/mod/irc.mod/msgcmds.c
@@ -656,7 +656,8 @@ static int msgc_help(Auth *a, char *chname, char *par)
   table = bind_table_lookup("msgc");
 
   for (entry = table->entries; entry && entry->next; entry = entry->next)
-    if (((chname[0] && entry->flags & AUTH_CHAN) || (!chname[0] && entry->flags & AUTH_MSG)) && flagrec_ok(&entry->user_flags, &fr))
+    if (((chname && chname[0] && (entry->cflags & AUTH_CHAN)) || 
+        (!(chname && chname[0]) && (entry->cflags & AUTH_MSG))) && flagrec_ok(&entry->user_flags, &fr))
       simple_snprintf(outbuf, sizeof(outbuf), "%s%s%s", outbuf[0] ? outbuf : "", outbuf[0] ? " " : "", &entry->function_name[6]);
 
   strncat(outbuf, "\n", sizeof(outbuf));
