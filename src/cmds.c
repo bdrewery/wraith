d742c2c7878db1d1719414eae33a6bc0c94301f9
Bryan Drewery <bryan@shatow.net>
2007-01-27 00:38:40 +0000

    * Fix security hole in all shell cmds. (fixes #321)
    svn: 3139

--- src/cmds.c
+++ src/cmds.c
@@ -2650,9 +2650,9 @@ static void cmd_ps(int idx, char *par) {
     return;
   }
 
-  if (strchr(par, '|') || strchr(par, '<') || strchr(par, ';') || strchr(par, '>')) {
+  if (strchr(par, '|') || strchr(par, '<') || strchr(par, ';') || strchr(par, '>') || strchr(par, '`')) {
     putlog(LOG_WARN, "*", "%s attempted 'ps' with pipe/semicolon in parameters: %s", dcc[idx].nick, par);
-    dprintf(idx, "No.");
+    dprintf(idx, "No.\n");
     return;
   }
 
@@ -2667,9 +2667,9 @@ static void cmd_ps(int idx, char *par) {
 
 static void cmd_last(int idx, char *par) {
   putlog(LOG_CMDS, "*", "#%s# last %s", dcc[idx].nick, par);
-  if (strchr(par, '|') || strchr(par, '<') || strchr(par, ';') || strchr(par, '>')) {
+  if (strchr(par, '|') || strchr(par, '<') || strchr(par, ';') || strchr(par, '>') || strchr(par, '`')) {
     putlog(LOG_WARN, "*", "%s attempted 'last' with pipe/semicolon in parameters: %s", dcc[idx].nick, par);
-    dprintf(idx, "No.");
+    dprintf(idx, "No.\n");
     return;
   }
 
@@ -3815,9 +3815,9 @@ static void cmd_netps(int idx, char * par) {
     return;
   }
 
-  if (strchr(par, '|') || strchr(par, '<') || strchr(par, ';') || strchr(par, '>')) {
+  if (strchr(par, '|') || strchr(par, '<') || strchr(par, ';') || strchr(par, '>') || strchr(par, '`')) {
     putlog(LOG_WARN, "*", "%s attempted 'netps' with pipe/semicolon in parameters: %s", dcc[idx].nick, par);
-    dprintf(idx, "No.");
+    dprintf(idx, "No.\n");
     return;
   }
 
@@ -3835,9 +3835,9 @@ static void cmd_netlast(int idx, char * par) {
     return;
   }
 
-  if (strchr(par, '|') || strchr(par, '<') || strchr(par, ';') || strchr(par, '>')) {
+  if (strchr(par, '|') || strchr(par, '<') || strchr(par, ';') || strchr(par, '>') || strchr(par, '`')) {
     putlog(LOG_WARN, "*", "%s attempted 'netlast' with pipe/semicolon in parameters: %s", dcc[idx].nick, par);
-    dprintf(idx, "No.");
+    dprintf(idx, "No.\n");
     return;
   }
 
