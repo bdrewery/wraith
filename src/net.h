4edddbe89a2f87155802bb776dd72fb8d519cd99
Bryan Drewery <bryan@shatow.net>
2007-02-28 22:52:06 +0000

    * The socket file generated for restarting is now encrypted.
    svn: 3209

--- src/net.h
+++ src/net.h
@@ -148,7 +148,7 @@ char *iptostr(in_addr_t);
 bool sock_has_data(int, int);
 int sockoptions(int sock, int operation, int sock_options);
 void init_net(void);
-int sock_read(FILE *);
+int sock_read(FILE *, bool);
 void sock_write(FILE *, int);
 bool socket_run();
 
