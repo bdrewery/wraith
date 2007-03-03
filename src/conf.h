a697c298a94db01108d2131c58aa8fe024806b81
Bryan Drewery <bryan@shatow.net>
2007-03-03 08:48:38 +0000

    * Fix 'cmd_conf change' to delete old bot first * Fix cmd_conf to act like confedit() and then simulate a SIGUSR1
    svn: 3222

--- src/conf.h
+++ src/conf.h
@@ -57,14 +57,14 @@ enum {
 
 void spawnbot(const char *);
 void spawnbots(conf_bot *bots, bool rehashed = 0);
-int conf_killbot(conf_bot *, const char *, conf_bot *, int);
+int conf_killbot(conf_bot *, const char *, conf_bot *, int, bool = 0);
 #ifdef __GNUC__
  void confedit() __attribute__((noreturn));
 #else
  void confedit();
 #endif
 void conf_addbot(char *, char *, char *, char *);
-int conf_delbot(char *);
+int conf_delbot(char *, bool kill = 1);
 pid_t checkpid(const char *, conf_bot *, const char *);
 void init_conf();
 void free_conf();
