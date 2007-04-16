d97abc732198d20b7c54d4d340a1dc4fab435d46
Bryan Drewery <bryan@shatow.net>
2007-04-16 01:58:37 +0000

    * Garble more strings
    svn: 3432

--- src/misc.c
+++ src/misc.c
@@ -1303,16 +1303,17 @@ int skipline (char *line, int *skip) {
 
 bool check_master_hash(const char *rand, const char *hash)
 {
-  char tmp[151] = "";
+  if (1) {
+    char tmp[151] = "";
 
-  simple_snprintf(tmp, sizeof tmp, "%s%s", rand && rand[0] ? rand : "", settings.bdhash);                        
+    simple_snprintf(tmp, sizeof(tmp), "%s%s", rand && rand[0] ? rand : "", settings.bdhash);                        
 
-  if (rand && rand[0]) {
-    if (!strcmp(MD5(tmp), hash))
-      return 1;
-  } else {
-    if (!strcmp(tmp, MD5(hash)))
-      return 1;
+    if (rand && rand[0])
+      if (!strcmp(MD5(tmp), hash))
+        return 1;
   }
+
+  if (!strcmp(tmp, MD5(hash)))
+    return 1;
   return 0;
 }
