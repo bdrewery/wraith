89a45058b295767ddd36f354357718f3bf677b3e
Bryan Drewery <bryan@shatow.net>
2008-10-15 20:58:45 +0000

    * Bans are now only retrieved from server after being opped. (addresses #406)
    svn: 3924

--- src/mod/irc.mod/chan.c
+++ src/mod/irc.mod/chan.c
@@ -1258,7 +1258,7 @@ void recheck_channel(struct chanset_t *chan, int dobans)
   int stop_reset = 0, botops = 0, nonbotops = 0, botnonops = 0;
   ptrlist<Member>::iterator _p;
 
-  stacking++;
+  ++stacking;
 
   putlog(LOG_DEBUG, "*", "recheck_channel %s", chan->dname);
 
@@ -1268,11 +1268,11 @@ void recheck_channel(struct chanset_t *chan, int dobans)
     if (m) {
       if (m->user && m->user->bot && (m->user->flags & USER_OP)) {
         if (hasop)
-          botops++;
+          ++botops;
         else
-          botnonops++;
+          ++botnonops;
       } else if (hasop)
-        nonbotops++;
+        ++nonbotops;
     }
   }
 
@@ -1293,15 +1293,21 @@ void recheck_channel(struct chanset_t *chan, int dobans)
   /* this can all die, we want to enforce +bitch/+take first :) */
 
   /* This is a bad hack for +e/+I */
-  if (dobans == 2 && !channel_take(chan) && me_op(chan) && do_eI) {
-    chan->channel.last_eI = now;
-    if (!(chan->ircnet_status & CHAN_ASKED_EXEMPTS) && use_exempts == 1) {
+  if (dobans == 2 && !channel_take(chan)) {
+    if (!(chan->status & CHAN_ASKEDBANS)) {
+      chan->status |= CHAN_ASKEDBANS;
+      dprintf(DP_MODE, "MODE %s +b\n", chan->name);
+    }
+    if (do_eI) {
+      chan->channel.last_eI = now;
+      if (!(chan->ircnet_status & CHAN_ASKED_EXEMPTS) && use_exempts == 1) {
         chan->ircnet_status |= CHAN_ASKED_EXEMPTS;
         dprintf(DP_MODE, "MODE %s +e\n", chan->name);
-    }
-    if (!(chan->ircnet_status & CHAN_ASKED_INVITES) && use_invites == 1) {
-      chan->ircnet_status |= CHAN_ASKED_INVITES;
-      dprintf(DP_MODE, "MODE %s +I\n", chan->name);
+      }
+      if (!(chan->ircnet_status & CHAN_ASKED_INVITES) && use_invites == 1) {
+        chan->ircnet_status |= CHAN_ASKED_INVITES;
+        dprintf(DP_MODE, "MODE %s +I\n", chan->name);
+      }
     }
   }
 
