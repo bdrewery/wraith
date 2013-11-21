2cd7028964a39c1bae95170f9f9f30d1f81617cd
Bryan Drewery <bryan@shatow.net>
2013-11-20 21:52:08 -0600

    Implement ctime, duration, myip, strftime, rand, unixtime

--- src/script_misc.cc
+++ src/script_misc.cc
@@ -24,6 +24,8 @@
  */
 
 #include "script.h"
+#include "main.h"
+#include "net.h"
 #include <bdlib/src/String.h>
 
 void script_putlog(const bd::String text) {
@@ -44,10 +46,100 @@ void script_putloglev(const bd::String levels, const bd::String channel, const b
   putlog(lev, channel.c_str(), text.c_str());
 }
 
+bd::String script_duration(int seconds) {
+  char s[70];
+  long tmp;
+
+  if (seconds <= 0)
+    return "0 seconds";
+
+  s[0] = 0;
+  if (seconds >= 31536000) {
+    tmp = (seconds / 31536000);
+    sprintf(s, "%lu year%s ", tmp, (tmp == 1) ? "" : "s");
+    seconds -= (tmp * 31536000);
+  }
+  if (seconds >= 604800) {
+    tmp = (seconds / 604800);
+    sprintf(&s[strlen(s)], "%lu week%s ", tmp, (tmp == 1) ? "" : "s");
+    seconds -= (tmp * 604800);
+  }
+  if (seconds >= 86400) {
+    tmp = (seconds / 86400);
+    sprintf(&s[strlen(s)], "%lu day%s ", tmp, (tmp == 1) ? "" : "s");
+    seconds -= (tmp * 86400);
+  }
+  if (seconds >= 3600) {
+    tmp = (seconds / 3600);
+    sprintf(&s[strlen(s)], "%lu hour%s ", tmp, (tmp == 1) ? "" : "s");
+    seconds -= (tmp * 3600);
+  }
+  if (seconds >= 60) {
+    tmp = (seconds / 60);
+    sprintf(&s[strlen(s)], "%lu minute%s ", tmp, (tmp == 1) ? "" : "s");
+    seconds -= (tmp * 60);
+  }
+  if (seconds > 0) {
+    tmp = (seconds);
+    sprintf(&s[strlen(s)], "%lu second%s", tmp, (tmp == 1) ? "" : "s");
+  }
+  if (strlen(s) > 0 && s[strlen(s) - 1] == ' ')
+    s[strlen(s) - 1] = 0;
+
+  return s;
+}
+
+bd::String script_unixtime() {
+  return bd::String::printf("%li", time(NULL));
+}
+
+bd::String script_ctime(long unixtime) {
+  time_t tt;
+
+  tt = (time_t) unixtime;
+  return ctime(&tt);
+}
+
+bd::String script_strftime(const bd::String format, long time) {
+  char buf[512];
+  struct tm *tm1;
+  time_t t;
+
+  if (time)
+    t = (time_t) time;
+  else
+    t = now;
+  tm1 = localtime(&t);
+  if (strftime(buf, sizeof(buf) - 1, format.c_str(), tm1))
+    return buf;
+  else
+    throw bd::String("error with strftime");
+}
+
+long script_myip() {
+  return getmyip();
+}
+
+unsigned long script_rand(long limit) {
+  if (limit <= 0)
+    throw bd::String("random limit must be greater than zero");
+  else if (limit > RANDOM_MAX)
+    throw bd::String::printf("random limit must be less than %li", (long)RANDOM_MAX);
+
+  return randint(limit);
+}
+
+
 void init_script_misc() {
-  script_add_command("putlog", script_putlog, "text");
-  script_add_command("putcmdlog", script_putcmdlog, "text");
-  script_add_command("putloglev", script_putloglev, "level(s) channel text");
+  script_add_command("ctime",		script_ctime,		"unixtime");
+  script_add_command("duration",	script_duration,	"seconds");
+  script_add_command("putlog",		script_putlog,		"text");
+  script_add_command("putcmdlog",	script_putcmdlog,	"text");
+  script_add_command("putloglev",	script_putloglev,	"level(s) channel text");
+  script_add_command("myip",		script_myip);
+  script_add_command("rand",		script_rand,		"limit");
+  script_add_command("strftime",	script_strftime,	"formatstring ?time?",		1);
+  script_add_command("unixtime",	script_unixtime);
 }
 
 /* vim: set sts=2 sw=2 ts=8 et: */
